5d7c470bc6fc466ba54fddf7576c62c5908d6d3c
msitsti2025 <msitsti2025@gmail.com>
2026-05-19 09:29:16 +0900

    Delete functions directory

--- functions/api/logout.js
+++
@@ -1,6 +0,0 @@
-import { makeExpiredCookie } from "../_lib/auth.js";
-import { json } from "../_lib/http.js";
-
-export function onRequestPost({ request }) {
-  return json({ ok: true }, 200, { "Set-Cookie": makeExpiredCookie(request) });
-}
